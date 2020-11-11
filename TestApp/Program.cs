e969bda853f8a4e6ebdd3213e1f0de6e60a543de
reg00 <vgrte@mail.ru>
2020-11-11 21:47:21 +0500

    1

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -17,7 +17,7 @@ namespace TestApp
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args) 
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
